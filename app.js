9bad350cf9ced44a6f7bd3050f3c464cdc5adf8d
Alenochka <alen4k@gmail.com>
2022-12-26 17:11:17 +0300

    cors added

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 const express = require('express');
-// const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const { errors } = require('celebrate');
-// const cors = require('cors');
+const cors = require('cors');
 const routes = require('./routes/index');
 const serverError = require('./utils/serverError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-// const options = {
-//   origin: [
-//     'http://localhost:3000',
-//     'https://alenadomain.students.nomoredomains.club',
-//     'http://alenadomain.students.nomoredomains.club',
-//     'https://alenakrestyaninova.github.io',
-//   ],
-//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ['Content-type', 'origin', 'Authorization'],
-//   credentials: true,
-// };
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'https://alenadiploma.nomoredomains.club',
+    'http://alenadiploma.nomoredomains.club',
+    'https://alenakrestyaninova.github.io',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
 const { PORT = 3000, MONGO_URL } = process.env;
 
@@ -29,10 +28,9 @@ mongoose.connect(MONGO_URL);
 
 const app = express();
 
-// app.use('*', cors(options));
+app.use('*', cors(options));
 
 app.use(express.json());
-// app.use(cookieParser());
 
 app.use(requestLogger);
 
